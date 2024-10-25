e109c9e52d4bdcec78583cdebce06bf05cb874e5
Marten <martinrages@gmail.com>
2024-10-25 18:28:42 +0200

    make example

--- Webserver/Program.cs
+++ Webserver/Program.cs
@@ -6,7 +6,7 @@ var serverSettings = new ServerSettings();
 serverSettings.Host = "0.0.0.0"; // Any
 serverSettings.HostV6 = "::"; // Any
 serverSettings.Port = 25501;
-serverSettings.RootDirectory = @"C:\Users\marti\Documents\repos\testing-site";
+serverSettings.RootDirectory = @"Example/";
 
 var server = new Server(serverSettings);
 server.Start();
